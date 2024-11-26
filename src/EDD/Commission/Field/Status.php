a3c8099272e28df50dd2686f045193ae6c27299b
David Sherlock <davidmichaelsherlock@icloud.com>
2024-11-26 15:14:31 +0300

    Refactoring

--- src/EDD/Commission/Field/Status.php
+++ src/EDD/Commission/Field/Status.php
@@ -2,7 +2,7 @@
 
 declare( strict_types=1 );
 
-namespace ArrayPress\Rules\EDD\Commision\Field;
+namespace ArrayPress\Rules\EDD\Commission\Field;
 
 use ArrayPress\Rules\Base\Options\Options;
 use function esc_html__;
