12677fca9e67b39ae5a19a2aaf94f7e624fc96a4
David Sherlock <davidmichaelsherlock@icloud.com>
2024-11-26 15:11:10 +0300

    Refactoring

--- src/Aliases/EDD/Discount.php
+++ src/Aliases/EDD/Discount.php
@@ -2,8 +2,8 @@
 declare( strict_types=1 );
 
 // Field
-class_alias( '\ArrayPress\Rules\EDD\Order\Field\Rate', '\ArrayPress\Rules\EDD\OrderRate' );
+class_alias( '\ArrayPress\Rules\EDD\Discount\Field\Rate', '\ArrayPress\Rules\EDD\DiscountRate' );
 
 // Search
-class_alias( '\ArrayPress\Rules\EDD\Order\Search\Discounts', '\ArrayPress\Rules\EDD\OrderSearchDiscounts' );
-class_alias( '\ArrayPress\Rules\EDD\Order\Search\Discount', '\ArrayPress\Rules\EDD\OrderSearchDiscount' );
+class_alias( '\ArrayPress\Rules\EDD\Discount\Search\Discount', '\ArrayPress\Rules\EDD\Discount' );
+class_alias( '\ArrayPress\Rules\EDD\Discount\Search\Discounts', '\ArrayPress\Rules\EDD\Discounts' );
